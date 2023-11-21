bdc0479c0accc0238aaf9d8bbeb168470e328751
23catur <caturwirasyahputra@gmail.com>
2023-11-21 13:59:44 +0800

    update

--- app/src/main/java/com/example/asset2/NavigasiActivity.java
+++ app/src/main/java/com/example/asset2/NavigasiActivity.java
@@ -129,13 +129,6 @@ public class NavigasiActivity extends AppCompatActivity {
         });
     }
 
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.navigasi, menu);
-//        return true;
-//    }
-
     @Override
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_navigasi);
--- app/src/main/java/com/example/asset2/SplashScreenActivity.java
+++ app/src/main/java/com/example/asset2/SplashScreenActivity.java
@@ -1,27 +1,49 @@
 package com.example.asset2;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
+    private  static  int SPLASH_TIMER = 3000;
+
+    SharedPreferences onBoardingScreen;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splashscreen);
-
-        int splash_screen = 3000;
 
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashScreenActivity.this, Login.class);
-                startActivity(intent);
-                finish();
+
+                onBoardingScreen = getSharedPreferences("onBoardingScreen",MODE_PRIVATE);
+                boolean isFirstTime = onBoardingScreen.getBoolean("firstTime", true);
+
+//                if (isFirstTime){
+//
+//                    SharedPreferences.Editor editor = onBoardingScreen.edit();
+//                    editor.putBoolean("firstTime", false);
+//                    editor.commit();
+//
+//                    Intent intent = new Intent(getApplicationContext(), Login.class);
+//                    startActivity(intent);
+//                    finish();
+//
+//                }else {
+//
+                    Intent intent = new Intent(getApplicationContext(), Login.class);
+                    startActivity(intent);
+                    finish();
+//                }
             }
-        }, splash_screen);
+        },SPLASH_TIMER);
     }
 }
